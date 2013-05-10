edda59f76d7394f8fa8ac66f5d9a9b1559364330
Jeremy H. Shi <shz6621091@hotmail.com>
2013-05-10 13:09:57 -0400

    fix 13b 3a bug

--- IMI-C/IMI-C/IMI-CQuestion4aDataViewController.m
+++ IMI-C/IMI-C/IMI-CQuestion4aDataViewController.m
@@ -34,7 +34,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     self.question4aTitle.text=NSLocalizedString(@"question4aTitle", nil);
-    self.question4aAnswerArray = [NSArray arrayWithObjects: NSLocalizedString(@"question4aAnswer1", nil),NSLocalizedString(@"question4aAnswer2", nil),NSLocalizedString(@"question4aAnswer0", nil),NSLocalizedString(@"question4aAnswer8", nil),nil];
+    self.question4aAnswerArray = [NSArray arrayWithObjects: NSLocalizedString(@"question4aAnswer8", nil),NSLocalizedString(@"question4aAnswer0", nil),NSLocalizedString(@"question4aAnswer1", nil),NSLocalizedString(@"question4aAnswer2", nil),nil];
     self.question4bTitle.text=NSLocalizedString(@"question4bTitle", nil);
     self.question4bAnswerArray = [NSArray arrayWithObjects: NSLocalizedString(@"question4bAnswer8", nil),NSLocalizedString(@"question4bAnswer0", nil),NSLocalizedString(@"question4bAnswer1", nil),nil];
 }
@@ -62,8 +62,8 @@
 - (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
 {
     BOOL isYes=row==1||row==0;
-    self.question4bTitle.hidden=!isYes;
-    self.question4bAnswer.hidden=!isYes;
+    self.question4bTitle.hidden=isYes;
+    self.question4bAnswer.hidden=isYes;
 }
 
 
@@ -77,7 +77,7 @@
     if (!question4aSelectedRow) {
         question4aAnswerValue=8;
     } else {
-        question4aAnswerValue=question4aSelectedRow;
+        question4aAnswerValue=question4aSelectedRow-1;
     }
     NSInteger question4bSelectedRow=[self.question4bAnswer selectedRowInComponent:0];
     if (!question4bSelectedRow) {
